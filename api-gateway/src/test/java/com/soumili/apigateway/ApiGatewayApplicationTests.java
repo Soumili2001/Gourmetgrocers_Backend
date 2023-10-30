b7c91d212ac47910196ac2062a7cf6d09f4270ea
Soumili Biswas <64208340+Soumili2001@users.noreply.github.com>
2023-10-30 22:56:42 +0530

    Add files via upload

---
+++ api-gateway/src/test/java/com/soumili/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumili.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
